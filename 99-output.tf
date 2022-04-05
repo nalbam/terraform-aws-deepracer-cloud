3e5c8bf2a6c181ec196cbc26b68a6962343c12ad
nalbam <me@nalbam.com>
2022-04-06 02:25:11 +0900

    add bucket output

--- 99-output.tf
+++ 99-output.tf
@@ -1,5 +1,13 @@
 # output
 
+output "bucket_local" {
+  value = aws_s3_bucket.local.id
+}
+
+output "bucket_upload" {
+  value = aws_s3_bucket.upload.id
+}
+
 output "public_ip" {
   value = aws_eip.worker.public_ip
 }
