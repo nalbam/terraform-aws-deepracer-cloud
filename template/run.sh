b469bd2b82a3e90ae6daded7dec71885f34daf8a
nalbam <me@nalbam.com>
2022-04-06 13:37:44 +0900

    bashrc

--- template/run.sh
+++ template/run.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/env bash
 
-CMD=$1
-
-if [ "${CMD}" == "init" ]; then
-  chmod 755 ~/run.sh
-  sudo cp ~/run.sh /etc/init.d/autostart.sh
-  sudo update-rc.d autostart.sh defaults
-  exit 0
-fi
-
 if [ -f ~/.autostarted ]; then
   exit 0
 fi
--- template/setup.sh
+++ template/setup.sh
@@ -46,11 +46,13 @@ lsblk
 
 apt-get install -y git vim tmux nmon
 
-curl -fsSL -o /etc/init.d/autostart.sh https://raw.githubusercontent.com/nalbam/terraform-aws-deepracer-local/main/template/autostart.sh
-chmod 755 /etc/init.d/autostart.sh
-update-rc.d autostart.sh defaults
+# curl -fsSL -o /etc/init.d/autostart.sh https://raw.githubusercontent.com/nalbam/terraform-aws-deepracer-local/main/template/autostart.sh
+# chmod 755 /etc/init.d/autostart.sh
+# update-rc.d autostart.sh defaults
 
 runuser -l ubuntu -c "curl -fsSL -o ~/run.sh https://raw.githubusercontent.com/nalbam/terraform-aws-deepracer-local/main/template/run.sh"
+runuser -l ubuntu -c "echo 'bash ~/run.sh' >> ~/.bashrc"
+
 runuser -l ubuntu -c "cd ~ && git clone https://github.com/aws-deepracer-community/deepracer-for-cloud.git"
 runuser -l ubuntu -c "cd ~/deepracer-for-cloud && ./bin/prepare.sh"
 
