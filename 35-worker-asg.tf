8af022a9307fa66dc848cad9b910ad4a23c7a1e4
nalbam <me@nalbam.com>
2022-10-18 23:57:07 +0900

    88-public-ip

--- 35-worker-asg.tf
+++ 35-worker-asg.tf
@@ -12,7 +12,7 @@ resource "aws_autoscaling_group" "worker" {
 
   suspended_processes = var.suspended_processes
 
-  target_group_arns = aws_lb_target_group.http.*.arn
+  # target_group_arns = aws_lb_target_group.http.*.arn
 
   launch_template {
     id      = aws_launch_template.worker.id
--- 53-route53.tf
+++ 53-route53.tf
@@ -1,29 +1,29 @@
-# aws_route53
+# # aws_route53
 
-data "aws_route53_zone" "worker" {
-  count = var.zone_name == "" ? 0 : 1
+# data "aws_route53_zone" "worker" {
+#   count = var.zone_name == "" ? 0 : 1
 
-  name = var.zone_name
-}
+#   name = var.zone_name
+# }
 
-resource "aws_route53_record" "worker" {
-  count = var.zone_name == "" ? 0 : 1
+# resource "aws_route53_record" "worker" {
+#   count = var.zone_name == "" ? 0 : 1
 
-  name    = var.name
-  type    = "A"
-  zone_id = data.aws_route53_zone.worker.0.zone_id
+#   name    = var.name
+#   type    = "A"
+#   zone_id = data.aws_route53_zone.worker.0.zone_id
 
-  allow_overwrite = true
+#   allow_overwrite = true
 
-  alias {
-    name                   = aws_lb.http.dns_name
-    zone_id                = aws_lb.http.zone_id
-    evaluate_target_health = "false"
-  }
-}
+#   alias {
+#     name                   = aws_lb.http.dns_name
+#     zone_id                = aws_lb.http.zone_id
+#     evaluate_target_health = "false"
+#   }
+# }
 
-# output
+# # output
 
-output "dns_name" {
-  value = try(aws_route53_record.worker.0.fqdn, "")
-}
+# output "dns_name" {
+#   value = try(aws_route53_record.worker.0.fqdn, "")
+# }
