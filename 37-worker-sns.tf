817c63df18ddacda38c18c6ed464d510085d0a5d
nalbam <me@nalbam.com>
2023-03-05 21:06:22 +0900

    rm sns

--- 37-worker-sns.tf
+++ 37-worker-sns.tf
@@ -1,23 +1,23 @@
 # aws_sns_topic
 
-resource "aws_sns_topic" "worker" {
-  name = format("%s-worker", var.name)
-}
+# resource "aws_sns_topic" "worker" {
+#   name = format("%s-worker", var.name)
+# }
 
-resource "aws_autoscaling_notification" "worker" {
-  group_names = [
-    aws_autoscaling_group.worker.name,
-  ]
+# resource "aws_autoscaling_notification" "worker" {
+#   group_names = [
+#     aws_autoscaling_group.worker.name,
+#   ]
 
-  notifications = [
-    "autoscaling:EC2_INSTANCE_LAUNCH",
-    "autoscaling:EC2_INSTANCE_TERMINATE",
-    "autoscaling:EC2_INSTANCE_LAUNCH_ERROR",
-    "autoscaling:EC2_INSTANCE_TERMINATE_ERROR",
-  ]
+#   notifications = [
+#     "autoscaling:EC2_INSTANCE_LAUNCH",
+#     "autoscaling:EC2_INSTANCE_TERMINATE",
+#     "autoscaling:EC2_INSTANCE_LAUNCH_ERROR",
+#     "autoscaling:EC2_INSTANCE_TERMINATE_ERROR",
+#   ]
 
-  topic_arn = aws_sns_topic.worker.arn
-}
+#   topic_arn = aws_sns_topic.worker.arn
+# }
 
 # resource "aws_sns_topic_subscription" "worker" {
 #   topic_arn = aws_sns_topic.worker.arn
